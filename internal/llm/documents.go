f65a442fa917f1216142901e71b68a3530bff39e
tanay-sgl <tanay.sagrolikar@gmail.com>
2024-08-05 18:27:42 +1000

    refactor_doc_search

---
+++ internal/llm/documents.go
@@ -0,0 +1,34 @@
+package llm
+
+import (
+	"fmt"
+	"orchestrator/internal/database"
+	"orchestrator/internal/models"
+	"strings"
+)
+
+func QueryUserRequestForSimilarDocuments(request models.LLMRAGQueryRequest, question string) (string, error) {
+	db, err := database.CreateDatabaseConnectionFromEnv()
+	if err != nil {
+		return "", err
+	}
+	defer db.Close()
+	var result strings.Builder
+	query_embedding, err := CreateEmbedding(request.Model, question)
+	if err != nil {
+		return "", err
+	}
+	similarDocuments, err := database.GetSimilaritySearchDocuments(db, query_embedding, request.SearchLimit)
+	if err != nil {
+		return "", err
+	}
+	for i, doc := range similarDocuments {
+		result.WriteString(fmt.Sprintf("Document %d:\n", i+1))
+		result.WriteString(fmt.Sprintf("Collection Slug: %s\n", doc.CollectionSlug))
+		result.WriteString(fmt.Sprintf("CID: %s\n", doc.CID))
+		result.WriteString(fmt.Sprintf("Content: %s\n", doc.Content))
+		result.WriteString("\n")
+	}
+
+	return result.String(), nil
+}
